eb4626b399f664d6855fac2100ac59f6906abd47
Daniel Weisser <daniel.weisser@gmail.com>
2020-05-29 19:23:02 +0200

    Initial test with ftp package

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,43 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Client} from 'qusly-core'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  const host: string = core.getInput('host')
+  const user: string = core.getInput('user')
+  const password: string = core.getInput('password')
+  const protocol: string = core.getInput('protocol')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  const client = new Client()
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  await client.connect({
+    host: 'www.example.com',
+    user: 'root',
+    password: 'password',
+    protocol: 'sftp'
+  })
+
+  const files = await client.readDir('/test')
+
+  core.debug(files.toString())
+
+  await client.disconnect()
 }
+
+// import {wait} from './wait'
+
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`)
+
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     core.setFailed(error.message)
+//   }
+// }
 
 run()
